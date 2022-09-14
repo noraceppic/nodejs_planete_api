5f0870e164d9e0f70934aca9d23800311cc041fd
noraceppic <messanora86@gmail.com>
2022-09-14 21:15:04 +0200

    Route planetes

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express =require('express');
+const sqlite3 = require("sqlite3").verbose();
+const dbname = "univers.db";
+
+
 
 const server=express();
 
+
+let db = new sqlite3.Database(dbname, (err) => {
+    if (err) throw err;
+    console.log("Base de donnée initialisé dans le fichier : " + dbname);
+  });
+
 server.get('/',function (req,res){
-    res.setHeader('content-type','text/html');
+    req.setHeader('content-type','text/html');
     res.status(200).send('<h1>Bienvenue dans l api  des planetes</h1>');
 })
 
 server.listen(7500,function(){
     console.log('server en marche');
 });
+
+server.get('/planetes', (req, res)=>{
+    res.setHeader('content-type','application/json');
+    db.all("SELECT * FROM planetes", (err, data) => {
+        if (err) throw err
+        res.status(200).send(JSON.stringify(data))
+      })
+   
+})
+
+;
